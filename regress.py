9ba9d5baba8888e07a2839e8666a632ac35f7903
Samuel Tenka <samtenka@umich.edu>
2020-08-05 21:17:00 -0400

    learning curves

--- regress.py
+++ regress.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+d = None
+N = None
+
 fd={'size':16, 'family':'serif'}
 
 ##
@@ -22,8 +25,8 @@ sneakers = (lbls==7)
 bags     = (lbls==8)
 boots    = (lbls==9)
 
-bottoms = sandals | boots | sneakers | trousers
-tops    = tees | pullovers | shirts | coats
+bottoms = sandals | boots | sneakers | trousers | bags
+tops    = tees | pullovers | shirts | coats | dresses
 
 intensities = np.mean(np.abs(imgs               ), axis=(1,2))
 diversities = np.mean(imgs*imgs, axis=(1,2)) - intensities**2
@@ -39,32 +42,35 @@ styles = {
 def plot_data(N, clothes, x_axis, y_axis, text, color):
     X, Y = x_axis[clothes][:N], y_axis[clothes][:N]
     plt.scatter(X, Y, **styles[color])
-    plt.text(np.mean(X)+0.1, np.mean(Y)-0.1, text, color=color, fontdict=fd)
+    plt.text(np.mean(X), np.mean(Y), text, color=color, fontdict=fd)
 
 def preprocess(x,y):
+    x = 4*x-2
+    y = 4*y-2
     return (
-        1.0+0*x, x, y, x*x, x*y, y*y, x*x*x, x*x*y, x*y*y, y*y*y, x*x*x*x, x*x*x*y, x*x*y*y, x*y*y*y, y*y*y*y
-        #1.0+0*x, x, y, x*x, x*y, y*y, x*x*x, x*x*y, x*y*y, y*y*y
-        #1.0+0*x, x, y
-    )
+        1.0+0*x,
+        x, y,
+        x*x, x*y, y*y,
+        x*x*x, x*x*y, x*y*y, y*y*y,
+        x*x*x*x, x*x*x*y, x*x*y*y, x*y*y*y, y*y*y*y,
+    )[:d]
 
-N = 50
-features = {
-    'tops':  np.array([ preprocess(x,y) for x,y in zip(backgrounds[tops  ][:N], asymmetries[tops  ][:N])]),
-    'bottoms':np.array([preprocess(x,y) for x,y in zip(backgrounds[bottoms][:N], asymmetries[bottoms][:N])]),
-}
+features = None 
+test_features = None 
 
 theta = np.zeros(len(preprocess(0,0)))
 def classify(data):
     return np.dot(data, theta)
 
-def badness():
-    return (  np.sum(np.maximum(0.0, +1.0 - classify(features['tops'])))
-            + np.sum(np.maximum(0.0, +1.0 + classify(features['bottoms'  ]))) )/(2*N)
+def badness(test=False):
+    ff = test_features if test else features
+    return (  np.sum(np.maximum(0.0, +1.0 - classify(ff['tops'])))
+            + np.sum(np.maximum(0.0, +1.0 + classify(ff['bottoms'  ]))) )/sum(map(len, ff.values()))
 
-def accuracy():
-    return (  np.sum(np.abs(classify(features['tops'])>0.0))
-            + np.sum(np.abs(classify(features['bottoms'])<0.0)) )/(2*N)
+def accuracy(test=False):
+    ff = test_features if test else features
+    return (  np.sum(np.abs(classify(ff['tops'])>0.0))
+            + np.sum(np.abs(classify(ff['bottoms'])<0.0)) )/sum(map(len, ff.values()))
 
 def squeaky():
     return (  np.sum(features['tops'][classify(features['tops']) < +1.0], axis=0)
@@ -82,7 +88,7 @@ def plot_class(db=True):
         Z = np.sign(Z)
 
     plt.imshow(Z, interpolation='nearest', extent=(0,1,0,1),
-               cmap='coolwarm', vmin=-12.0, vmax=+12.0)
+               cmap='coolwarm', vmin=-8.0, vmax=+8.0)
 
 def save_plot(file_name):
     plt.xticks([])
@@ -90,30 +96,94 @@ def save_plot(file_name):
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
 
-    plt.xlabel('backgrounds', fontdict=fd)
-    plt.ylabel('asymmetries', fontdict=fd)
+    plt.xlabel('percent background', fontdict=fd)
+    plt.ylabel('asymmetry', fontdict=fd)
     plt.gca().set_aspect('equal')
-    plt.gcf().set_size_inches(8.0, 8.0)
+    plt.gcf().set_size_inches(5.0, 5.0)
     plt.tight_layout()
     
     plt.savefig(file_name)
     plt.clf()
 
-steps_taken = 0
-for i in range(20):
-    nb_steps = 20*(i+1)
-    for _ in range(nb_steps):
-        theta = theta + 0.1 * squeaky()
-    steps_taken += nb_steps 
 
-    print('badness {:6.2f}% ... accuracy {:3.0f}%'.format(
-          100*badness(), 100*accuracy()), end='   ')
-    print(' '.join('{:+2.0f}'.format(xx) for xx in theta))
+for NN in [12, 24, 48]:
+    dds = list(range(1,15+1))
+    tests = []
+    bads = []
+    trains = []
+    for dd in dds:
+        d = dd
+        N = NN
 
-    for db in [True, False]:
-        plot_data(N, tops  ,   backgrounds, asymmetries, 'tops', 'red')
-        plot_data(N, bottoms, backgrounds, asymmetries, 'bottoms', 'blue')
-        plot_class(db)
-        plt.text(0.02, 0.95, 'after {:3d} steps'.format(steps_taken), fontdict=fd)
-        save_plot('yo-{:02d}-{:s}.png'.format(i, ['sm','bd'][db]))
+        features = {
+            'tops':  np.array([ preprocess(x,y) for x,y in zip(backgrounds[tops  ][:N], asymmetries[tops  ][:N])]),
+            'bottoms':np.array([preprocess(x,y) for x,y in zip(backgrounds[bottoms][:N], asymmetries[bottoms][:N])]),
+        }
+        test_features = {
+            'tops':  np.array([ preprocess(x,y) for x,y in zip(backgrounds[tops  ][-1000:], asymmetries[tops  ][-1000:])]),
+            'bottoms':np.array([preprocess(x,y) for x,y in zip(backgrounds[bottoms][-1000:], asymmetries[bottoms][-1000:])]),
+        }
 
+        steps = []
+        badnesses = []
+        err_trains = []
+        err_tests = []
+        
+        theta = np.zeros(len(preprocess(0,0)))
+
+        step = 0
+        while step != 5000:
+            theta = theta + 0.01 * squeaky()
+            step += 1
+            
+            if step % 2: continue
+        
+            bb = badness()
+            e_train = 1.0-accuracy()
+            e_test = 1.0-accuracy(test=True)
+        
+            steps.append(step)
+            badnesses.append(bb)
+            err_trains.append(e_train)
+            err_tests.append(e_test)
+        
+            if step not in [10, 200, 4000]: continue
+            if (d,N) not in ((10, 12), (3, 48), (15, 48)): continue
+        
+            print('badness {:6.2f}% ... accuracy {:3.0f}% ... theta {:s}'.format(
+                100*bb, 100*e_train, ' '.join('{:+2.0f}'.format(xx) for xx in theta)
+            ))
+        
+            for db in [True, False]:
+                plot_data(N, tops  ,  backgrounds, asymmetries, 'tops', 'red')
+                plot_data(N, bottoms, backgrounds, asymmetries, 'bottoms', 'blue')
+                plot_class(db)
+                plt.text(0.02, 0.95, 'after {:3d} steps'.format(step), fontdict=fd)
+                save_plot('yo-{:02d}-{:04d}-{:s}-{:02d}.png'.format(d, N, ['soft','hard'][db], step))
+        
+        bads.append(badnesses[-1])
+        trains.append(err_trains[-1])
+        tests.append(err_tests[-1])
+
+        if N != 12 or d not in (3, 6,10,15,28): continue
+        plt.plot(np.array(steps), err_tests , c='blue')
+        plt.plot(np.array(steps), err_trains, c='red')
+        plt.plot(np.array(steps), badnesses , c='orange')
+        plt.xscale('log')
+        plt.ylim([0.0, 0.5])
+        plt.yticks([0.0, 0.2, 0.4])
+        plt.xlabel('(log) number of optimization updates', fontdict=fd)
+        plt.tight_layout()
+        plt.savefig('yoyo-{:02d}-{:04d}.png'.format(d,N))
+        plt.clf()
+
+    plt.plot(dds, np.array(trains), c='red')
+    plt.plot(dds, np.array(tests), c='blue')
+    plt.plot(dds, np.array(bads), c='orange')
+    plt.ylim(([0.0, 0.5]))
+    plt.yticks(([0.0, 0.2, 0.4]))
+    plt.xticks(([1,3,6,10,15]))
+    plt.xlabel('data dimension after preprocessing', fontdict=fd)
+    plt.tight_layout()
+    plt.savefig('yoyoyo-{:04d}.png'.format(N))
+    plt.clf()
