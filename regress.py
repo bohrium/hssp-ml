27cfa17f477792d9e494ea02ea143d37a58c518d
Samuel Tenka <samtenka@umich.edu>
2020-08-06 20:02:05 -0400

    improve overfit 2x2

--- regress.py
+++ regress.py
@@ -25,15 +25,16 @@ sneakers = (lbls==7)
 bags     = (lbls==8)
 boots    = (lbls==9)
 
-bottoms = sandals | boots | sneakers | trousers | bags
+bottoms = sandals | boots | sneakers | trousers #| bags
 tops    = tees | pullovers | shirts | coats | dresses
 
 intensities = np.mean(np.abs(imgs               ), axis=(1,2))
 diversities = np.mean(imgs*imgs, axis=(1,2)) - intensities**2
 bottom_seps = np.mean(np.abs(imgs[:,14:,12:16]  ), axis=(1,2))
-asymmetries = np.mean(np.abs(imgs-imgs[:,:,::-1]), axis=(1,2)) / intensities
+asymmetries = np.mean(np.abs(imgs-imgs[:,:,::-1]), axis=(1,2)) / (2*intensities)
 heavinesses = np.mean(np.abs(imgs[:,:14,:]      ), axis=(1,2))
-backgrounds = np.mean(       imgs < 0.05         , axis=(1,2))
+#backgrounds = np.mean(       imgs < 0.05         , axis=(1,2))
+backgrounds = np.mean(       imgs < 0.01         , axis=(1,2))
 
 styles = {
     'red':  {'marker':'+', 'c':'red', 's':60},
@@ -42,11 +43,13 @@ styles = {
 def plot_data(N, clothes, x_axis, y_axis, text, color):
     X, Y = x_axis[clothes][:N], y_axis[clothes][:N]
     plt.scatter(X, Y, **styles[color])
-    plt.text(np.mean(X)-0.3, np.mean(Y), text, color=color, fontdict=fd)
+    plt.text(0.9-len(text)*0.02, np.mean(Y)+0.05, text, color=color, fontdict=fd)
 
 def preprocess(x,y):
-    x = 3*x-1.5
-    y = 3*y-1.5
+    x = 4*x-2
+    y = 4*y-2
+    #x = 3*x-1.5
+    #y = 3*y-1.5
     return (
         1.0+0*x,
         y, x,
@@ -88,9 +91,11 @@ def plot_class(db=True):
     Z = classify(Z)
     if db:
         Z = np.sign(Z)
+    Z = np.maximum(-3.0, np.minimum(3.0, Z))
 
+    fadedness = 8.0 if db else 16.0
     plt.imshow(Z, interpolation='nearest', extent=(0,1,0,1),
-               cmap='coolwarm', vmin=-8.0, vmax=+8.0)
+               cmap='coolwarm', vmin=-fadedness, vmax=+fadedness)
 
 def save_plot(file_name):
     plt.xticks([])
@@ -113,10 +118,10 @@ def save_plot(file_name):
     plt.clf()
 
 
-for NN in [12, 24, 48,96]:
+for NN in [2, 16, 64]:
     N = NN
 
-    dds = list(range(1,28+1)) if N==12 else [2,3,6,10,21,28]
+    dds = list(range(1,28+1)) if N==16 else [2,3,6,10,21,28]
     tests = []
     bads = []
     trains = []
@@ -127,9 +132,10 @@ for NN in [12, 24, 48,96]:
             'tops':  np.array([ preprocess(x,y) for x,y in zip(backgrounds[tops  ][:N], asymmetries[tops  ][:N])]),
             'bottoms':np.array([preprocess(x,y) for x,y in zip(backgrounds[bottoms][:N], asymmetries[bottoms][:N])]),
         }
+        TEST = 5000
         test_features = {
-            'tops':  np.array([ preprocess(x,y) for x,y in zip(backgrounds[tops  ][-1000:], asymmetries[tops  ][-1000:])]),
-            'bottoms':np.array([preprocess(x,y) for x,y in zip(backgrounds[bottoms][-1000:], asymmetries[bottoms][-1000:])]),
+            'tops':  np.array([ preprocess(x,y) for x,y in zip(backgrounds[tops  ][-TEST:], asymmetries[tops  ][-TEST:])]),
+            'bottoms':np.array([preprocess(x,y) for x,y in zip(backgrounds[bottoms][-TEST:], asymmetries[bottoms][-TEST:])]),
         }
 
         steps = []
@@ -144,19 +150,19 @@ for NN in [12, 24, 48,96]:
             theta = theta + 0.01 * squeaky()
             step += 1
             
-            if step % 2: continue
+            if step % 25 and step not in [10, 1,2,4,8,16,32,64,128,256]: continue
         
             bb = badness()
-            e_train = 1.0-accuracy()
-            e_test = 1.0-accuracy(test=True)
+            e_train = 1.0 - accuracy()
+            e_test  = 1.0 - accuracy(test=True)
         
             steps.append(step)
             badnesses.append(bb)
             err_trains.append(e_train)
             err_tests.append(e_test)
         
-            if step not in [10, 100, 1000, 10000]: continue
-            if (d,N) not in ((2,12), (28, 12), (2, 96), (28, 96)): continue
+            if step not in [1, 10, 100, 10000]: continue
+            if (d,N) not in ((2,2), (2, 64), (10, 16), (28,2), (28, 64)): continue
         
             print('badness {:6.2f}% ... accuracy {:3.0f}% ... theta {:s}'.format(
                 100*bb, 100*e_test, ' '.join('{:+2.0f}'.format(xx) for xx in theta)
@@ -166,7 +172,7 @@ for NN in [12, 24, 48,96]:
                 plot_data(N, tops  ,  backgrounds, asymmetries, 'tops', 'red')
                 plot_data(N, bottoms, backgrounds, asymmetries, 'bottoms', 'blue')
                 plot_class(db)
-                plt.text(0.02, 0.95, 'after {:3d} steps, accuracy {:.1f}%'.format(
+                plt.text(0.02, 0.88, 'after {:3d} steps\ntest acc {:.0f}%'.format(
                     step, 100*(1.0-e_test)), fontdict=fd)
                 save_plot('yo-{:02d}-{:04d}-{:s}-{:02d}.png'.format(d, N, ['soft','hard'][db], step))
         
@@ -174,13 +180,19 @@ for NN in [12, 24, 48,96]:
         trains.append(err_trains[-1])
         tests.append(err_tests[-1])
 
-        if N != 12 or d not in (3,6,10,15,28): continue
-        plt.plot(np.array(steps), err_tests , c='blue')
-        plt.plot(np.array(steps), err_trains, c='red')
-        plt.plot(np.array(steps), badnesses , c='orange')
+        if N != 16 or d not in (3,6,10,15,28): continue
+        steps = np.array(steps)
+        plt.plot(steps, err_tests , c='blue')
+        plt.plot(steps, err_trains, c='red')
+        plt.plot(steps, badnesses , c='orange')
+
+        best_val, best_idx = min((v,i) for i,v in enumerate(err_tests))
+        plt.plot([steps[best_idx], max(steps)     ], [best_val, best_val], c='blue', linestyle='dashed')
+        plt.plot([steps[best_idx], steps[best_idx]], [0.0     , best_val], c='blue', linestyle='dashed')
+
         plt.xscale('log')
-        plt.ylim([0.0, 0.5])
-        plt.yticks([0.0, 0.2, 0.4])
+        plt.ylim([0.0, 0.1])
+        plt.yticks([0.0, 0.1])
         plt.xlabel('(log) number of optimization updates', fontdict=fd)
         plt.gca().spines['right'].set_visible(False)
         plt.gca().spines['top'].set_visible(False)
@@ -189,12 +201,19 @@ for NN in [12, 24, 48,96]:
         plt.savefig('yoyo-{:02d}-{:04d}.png'.format(d,N))
         plt.clf()
 
-    plt.plot(dds, np.array(trains), c='red')
-    plt.plot(dds, np.array(tests), c='blue')
-    plt.plot(dds, 0*np.array(tests) + min(tests), c='blue', linestyle='dashed')
+    tests = np.array(tests)
+    trains = np.array(trains)
+    dds = np.array(dds)
+    plt.plot(dds, trains, c='red')
+    plt.plot(dds, tests, c='blue')
     plt.plot(dds, np.array(bads), c='orange')
-    plt.ylim(([0.0, 0.5]))
-    plt.yticks(([0.0, 0.2, 0.4]))
+
+    best_val, best_idx = min((v,i) for i,v in enumerate(tests))
+    plt.plot([dds[best_idx], max(dds)],      [best_val, best_val], c='blue', linestyle='dashed')
+    plt.plot([dds[best_idx], dds[best_idx]], [0.0, best_val],      c='blue', linestyle='dashed')
+
+    plt.ylim(([0.0, 0.1]))
+    plt.yticks(([0.0, 0.1]))
     plt.xticks(([1,3,6,10,15,21,28]))
     plt.xlabel('data dimension after preprocessing', fontdict=fd)
     plt.gca().spines['right'].set_visible(False)
